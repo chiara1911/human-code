4b8ec88651363dad85e55b15dbd51e66fc40876e
chiara1911 <chiaracesari89@gmail.com>
2023-10-05 19:28:08 +0200

    creazione passaggi per  un task specifico

---
+++ script.js
@@ -0,0 +1,62 @@
+// Human Code es. 
+//  - Fare la spesa seguendo una lista-  
+ 
+//  1.Prendere carta e penna 
+//  2.Aprire il frigo 
+//  3.Controllare oggetto mancante 
+//    3.1 Annotare oggetto mancante 
+//    3.2 Ripetere punto 3 finchè non sono stati annotati tutti gli oggetti mancanti 
+ 
+//  4.Chiudere il frigo 
+//  5.Aprire la dispensa 
+//  6.Controllare oggetto mancante 
+//    6.1 Annotare oggetto mancante 
+//    6.2 Ripetere punto 6 finchè non sono stati annotati tutti gli oggetti mancanti 
+ 
+//  7.Chiudere la dispensa 
+//  8.Aprire dispensa Ricky 
+//  9.Controllare se ci sono le crocchette 
+//    9.1 Se non ci sono, annotare 
+ 
+//  10.Chiudere dispensa Ricky 
+//  11.Controllare portafogli 
+//    11.1 Controllare coupon 
+//    11.2 Se non ci sono, aggiungerli nel portafogli 
+//    11.3 Controllare moneta per carrello 
+//    11.4 Se non c'è, aggiungere nel portafogli 
+ 
+//  12.Prepararsi per uscire 
+//  13.Prendere la lista 
+//  14.Uscire di casa 
+//  15.Dirigersi al supermercato 
+//  16.Andare dove ci sono i carrelli 
+//    16.1 Controllare se ci sono carrelli disponibili 
+ 
+//  17. Se dispo. prendere carrello 
+//    17.1 Altrimenti prendere un cestino  
+  
+//  18.Entrare dentro al supermercato 
+//  19.Controllare la lista 
+//   19.1 Leggere primo oggetto mancante 
+//   19.2 Cerca primo oggetto mancante 
+//   19.2.1 Se non c'è, prendi prodotto simile. 
+//   19.2.2 Se non c'è prodotto simile non prendere niente. 
+// 19.3 Metti nel carrello 
+//   19.3.1 Eliminare dalla lista l'oggetto trovato/ non trovato 
+// 19.4 Ripeti dal punto 19.1 finchè la lista non si esaurisce. 
+// 19.5 Se l'oggetto crocchette non c'è, vai in un altro supermercato e comprale dopo il punto 27. 
+
+// 20.Prendere due extra snacks 
+// 21.Dirigersi in cassa 
+// 22.Inserire oggetti sul nastro 
+// 23.Attendere il totale da pagare 
+// 24.Utilizzare coupon 
+// 25.Aprire portafogli 
+// 26.Controllare se ci sono abbastanza contanti 
+// 26.1.Se bastano al punto successivo 
+// 26.2 Se non bastano mettere da parte gli snacks 
+// 26.3 Se i contanti continuano a non bastare, allora togliere dalla spesa alcuni oggetti, ma non le crocchette! 
+
+// 27.Pagare 
+
+
